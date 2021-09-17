efd1f24b7ba9eedf5be398c345ddf42f039c69ab
mafei <mafei.dev@gmail.com>
2021-09-18 01:15:11 +0530

    Initial commit

---
+++ src/test/java/com/mafei/websocket/SpringWebFluxWebsockertAdvancedSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mafei.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFluxWebsockertAdvancedSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

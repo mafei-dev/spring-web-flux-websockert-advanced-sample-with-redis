b662bbdf352f7fe826e104e7690870a78e141ebf
mafei <mafei.dev@gmail.com>
2021-09-18 02:53:01 +0530

    added redis pub sub

--- src/main/java/com/mafei/websocket/service/SWService.java
+++ src/main/java/com/mafei/websocket/service/SWService.java
@@ -1,8 +1,13 @@
 package com.mafei.websocket.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RTopicReactive;
+import org.redisson.api.RedissonReactiveClient;
+import org.redisson.client.codec.StringCodec;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
@@ -16,9 +21,9 @@ import java.util.Arrays;
 @Slf4j
 public class SWService implements WebSocketHandler {
 
-    /*   @Autowired
-       private RedissonReactiveClient client;
-   */
+    @Autowired
+    private RedissonReactiveClient client;
+
     public static Flux<String> sendMessage() {
         return Flux.interval(Duration.ofSeconds(1)).map(aLong -> "hi " + aLong).startWith(Arrays.asList("your welcome."));
     }
@@ -28,7 +33,7 @@ public class SWService implements WebSocketHandler {
         String service = getParamFromURL(webSocketSession.getHandshakeInfo().getUri(), "service");
         log.info("service name is {}", service);
         log.info("info {} ", webSocketSession.getHandshakeInfo());
-        /*RTopicReactive healthTopic = client.getTopic("health", StringCodec.INSTANCE);
+        RTopicReactive healthTopic = client.getTopic("health", StringCodec.INSTANCE);
 
         webSocketSession.receive()
                 .map(WebSocketMessage::getPayloadAsText)
@@ -40,17 +45,15 @@ public class SWService implements WebSocketHandler {
                 .doFinally(signalType -> {
                     System.out.println("receive.doFinally");
                 })
-                .subscribe();*/
-       /* Flux<WebSocketMessage> fluxMsg = healthTopic.getMessages(String.class)
+                .subscribe();
+        Flux<WebSocketMessage> fluxMsg = healthTopic.getMessages(String.class)
                 .map(webSocketSession::textMessage)
                 .doOnError(Throwable::printStackTrace)
                 .doFinally(signalType -> {
                     System.out.println("send.doFinally");
-                });*/
+                });
 
-        return webSocketSession.send(sendMessage()
-                .startWith(Arrays.asList("time is good"))
-                .map(webSocketSession::textMessage));
+        return webSocketSession.send(fluxMsg);
     }
 
     private String getParamFromURL(URI uri, String param) {
